6c7e4a3fbaefc851c398f5e4f6733abc87657706
Andri Manuel Zabala Urbaez <andriurbaez05@gmail.com>
2025-06-22 22:22:15 -0400

    Coloque la cadena de conexion en un archivo txt, agregue un nuevo modelo de datos y la consulta para la api

---
+++ MovieCLI/Models/Movies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCLI.Models
+{
+    public class Movies
+    {
+        public Movies(string request)
+        {
+            switch (request)
+            {
+                case "tmdb-app --type \"playing\"\r\n":
+                    NowPlaying();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        void NowPlaying()
+        {
+
+        }
+    }
+}
